f1be125bfc06d64ee8aa04bd1cbc7c66a74e85b1
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2023-09-12 16:45:41 +0200

    Fixed: code style
    SubcolumnTypes.php

--- modules/SubcolumnTypes.php
+++ modules/SubcolumnTypes.php
@@ -2,21 +2,25 @@
 
 namespace HeimrichHannot\Subcolumns;
 
+use HeimrichHannot\SubColumnsBootstrapBundle\SubColumnsBootstrapBundle;
+
 class SubcolumnTypes
 {
-    private static $strSet = null;
+    private static string $strSet;
 
-    public static function compatSetType($default = 'yaml3'): string
+    public static function compatSetType(string $default = 'yaml3'): string
     {
-        if (is_string(self::$strSet)) return self::$strSet;
-
-        $strSet = $GLOBALS['TL_CONFIG']['subcolumns'] ?? $default;
-
-        if (class_exists('\HeimrichHannot\SubColumnsBootstrapBundle\SubColumnsBootstrapBundle')) {
-            $strSet = \HeimrichHannot\SubColumnsBootstrapBundle\SubColumnsBootstrapBundle::validateTypeString($strSet) ?? $strSet;
+        if (isset(self::$strSet)) {
+            return self::$strSet;
         }
 
-        return $strSet;
+        self::$strSet = $GLOBALS['TL_CONFIG']['subcolumns'] ?? $default;
+
+        if (class_exists(SubColumnsBootstrapBundle::class)) {
+            self::$strSet = SubColumnsBootstrapBundle::validateTypeString(self::$strSet) ?? self::$strSet;
+        }
+
+        return self::$strSet;
     }
 
 }
